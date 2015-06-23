b15b83f7af1f3bdd64681b52b6e36b692d539d42
Joseph Burnett <josephburnett79@gmail.com>
2015-06-23 12:25:07 -0700

    stateless design

--- main.go
+++ main.go
@@ -9,9 +9,51 @@ import (
         "gopkg.in/amz.v1/s3"
 )
 
-var bucket *s3.Bucket
-
 func main() {
+
+        // We want the put the same sequence of objects so we don't accumulate storage
+        rand.Seed(0)
+
+        // Limit determines how fast objects are put.
+        limit := make(chan bool)
+        goHome := make(chan bool)
+
+        // 100 partiers :)
+        for i := 0; i < 100; i++ {
+                go putParty(limit);
+        }
+
+        // 1 party pooper :(
+        go partyPooper(limit, goHome)
+        <- goHome
+}
+
+func putParty(limit chan bool) {
+
+        // Put to the max!
+        for {
+                <-limit
+                put()
+        }
+}
+
+func partyPooper(limit, goHome chan bool) {
+
+        // Don't party too fast!  Don't party too long!
+        start := time.Now()
+        for {
+                elapsed := time.Since(start)
+                if (elapsed > 1000000000 * 60 * 5 /* 5 minutes */) {
+                        fmt.Printf("\n")
+                        goHome <- true
+                }
+
+                limit <- true
+                time.Sleep(50 * time.Millisecond)
+        }
+}
+
+func put() {
 
         // The AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables are used.
         auth, err := aws.EnvAuth()
@@ -21,50 +63,17 @@ func main() {
 
         // Open Bucket
         s := s3.New(auth, aws.USEast)
-        bucket = s.Bucket("joburnet-lambda-sources")
-
-        rand.Seed(0)
-
-        limit := make(chan bool)
-        done := make(chan bool)
-
-        for i := 0; i < 50; i++ {
-                go putParty(limit);
-        }
-
-        go partyPooper(limit, done)
-        <- done
-}
-
-func putParty(limit chan bool) {
-        for {
-                <-limit
-                put()
-        }
-}
-
-func partyPooper(limit, done chan bool) {
-
-        for i := 0; i < 100; i++ {
-                limit <- true
-                time.Sleep(100 * time.Millisecond)
-        }
-
-        fmt.Printf("\n")
-        done <- true
-}
-
-func put() {
+        bucket := s.Bucket("joburnet-lambda-sources")
 
         data := []byte("bang!")
         key := randSeq(32)
 
-        err := bucket.Put(key, data, "content-type", s3.Private)
+        err = bucket.Put(key, data, "content-type", s3.Private)
         if err != nil {
-                panic(err)
+                fmt.Printf("%v", err)
+        } else {
+                fmt.Printf(".")
         }
-
-        fmt.Printf(".")
 }
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
